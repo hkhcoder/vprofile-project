2b193ac20289b2276522bcf4199f88f24124d0cd
hkhcoder <137309866+hkhcoder@users.noreply.github.com>
2023-12-03 19:35:10 +0400

    Update rabbitmq.sh

--- userdata/rabbitmq.sh
+++ userdata/rabbitmq.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-sudo yum install epel-release -y
-sudo yum update -y
-sudo yum install wget -y
-cd /tmp/
-dnf -y install centos-release-rabbitmq-38
- dnf --enablerepo=centos-rabbitmq-38 -y install rabbitmq-server
- systemctl enable --now rabbitmq-server
- firewall-cmd --add-port=5672/tcp
- firewall-cmd --runtime-to-permanent
-sudo systemctl start rabbitmq-server
-sudo systemctl enable rabbitmq-server
-sudo systemctl status rabbitmq-server
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash
+sudo yum makecache -y --disablerepo='*' --enablerepo='rabbitmq_rabbitmq-server'
+sudo yum -y install rabbitmq-server
+rpm -qi rabbitmq-server 
+systemctl start rabbitmq-server
+systemctl enable rabbitmq-server
 sudo sh -c 'echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config'
 sudo rabbitmqctl add_user test test
 sudo rabbitmqctl set_user_tags test administrator
 sudo systemctl restart rabbitmq-server
+firewall-cmd --add-port=5672/tcp
+firewall-cmd --runtime-to-permanent
