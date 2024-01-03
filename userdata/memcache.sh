6b6ed5d8821a18f4649860c7dc10469524fb9f67
hkhcoder <137309866+hkhcoder@users.noreply.github.com>
2024-01-03 21:29:14 +0400

    Firewalld

--- userdata/memcache.sh
+++ userdata/memcache.sh
@@ -6,6 +6,9 @@ sudo systemctl enable memcached
 sudo systemctl status memcached
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/sysconfig/memcached
 sudo systemctl restart memcached
+sudo yum install firewalld -y
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
 firewall-cmd --add-port=11211/tcp
 firewall-cmd --runtime-to-permanent
 firewall-cmd --add-port=11111/udp
