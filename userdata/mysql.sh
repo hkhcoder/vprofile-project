77bec99f65e6c71d63eb00ba3a98f37062e0c016
hkhcoder <137309866+hkhcoder@users.noreply.github.com>
2024-06-22 11:41:06 +0530

    Update mysql.sh

--- userdata/mysql.sh
+++ userdata/mysql.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 DATABASE_PASS='admin123'
 sudo yum update -y
-sudo yum install epel-release -y
+#sudo yum install epel-release -y
 sudo yum install git zip unzip -y
-sudo yum install mariadb-server -y
-
-
+sudo dnf install mariadb105-server -y
 # starting & enabling mariadb-server
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
@@ -13,7 +11,7 @@ cd /tmp/
 git clone -b main https://github.com/hkhcoder/vprofile-project.git
 #restore the dump file for the application
 sudo mysqladmin -u root password "$DATABASE_PASS"
-sudo mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DATABASE_PASS'"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
@@ -23,18 +21,3 @@ sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 
 sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'%' identified by 'admin123'"
 sudo mysql -u root -p"$DATABASE_PASS" accounts < /tmp/vprofile-project/src/main/resources/db_backup.sql
 sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
-
-# Restart mariadb-server
-sudo systemctl restart mariadb
-
-
-#starting the firewall and allowing the mariadb to access from port no. 3306
-sudo yum install firewalld -y
-sudo systemctl start firewalld
-sudo systemctl enable firewalld
-sudo systemctl start firewalld
-sudo systemctl enable firewalld
-sudo firewall-cmd --get-active-zones
-sudo firewall-cmd --zone=public --add-port=3306/tcp --permanent
-sudo firewall-cmd --reload
-sudo systemctl restart mariadb
