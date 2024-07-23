ba8cdd8b8846fec62bba626e9402365ffb94bbd1
Kashif Shehzad Baloch <kashif.shehzad.725@gmail.com>
2024-07-23 05:55:27 +0500

    fixed Unit tomcat9.service could not be found

--- userdata/tomcat_ubuntu.sh
+++ userdata/tomcat_ubuntu.sh
@@ -2,4 +2,4 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install openjdk-11-jdk -y
-sudo apt install tomcat9 tomcat9-admin tomcat9-docs tomcat9-common git -y
+sudo apt install tomcat10 tomcat10-admin tomcat10-docs tomcat10-common git -y
