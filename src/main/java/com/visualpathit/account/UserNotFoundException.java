6123445b11eff9f632640ef0f18f4573da5e353c
imranteli <imranteli@bitbucket.org>
2024-08-20 09:47:25 +0530

    sel autoscripts

--- src/main/java/com/visualpathit/account/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.visualpathit.account;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
