4f240047c4e31a5f9e3dbdb9019c199d55d33cdf
Imran teli <hkhcoder@gmail.com>
2024-09-10 17:44:44 +0530

    br update

--- vagrant/Automated_provisioning_WinMacIntel/tomcat.sh
+++ vagrant/Automated_provisioning_WinMacIntel/tomcat.sh
@@ -48,7 +48,7 @@ systemctl daemon-reload
 systemctl start tomcat
 systemctl enable tomcat
 
-git clone -b local-setup https://github.com/hkhcoder/vprofile-project.git
+git clone -b local https://github.com/hkhcoder/vprofile-project.git
 cd vprofile-project
 mvn install
 systemctl stop tomcat
