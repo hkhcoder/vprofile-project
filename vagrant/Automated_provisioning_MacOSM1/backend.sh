ca7f1bbe7a90353302f9c07ac19b8ee082fd6171
pavan-official <pavan.nag.anganna@gmail.com>
2024-10-14 16:57:27 -0500

    changed DB from fedora to ubuntu

--- vagrant/Automated_provisioning_MacOSM1/backend.sh
+++ vagrant/Automated_provisioning_MacOSM1/backend.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 DATABASE_PASS='admin123'
 
-# MEmcache
-yum install epel-release -y
-yum install memcached -y
-systemctl start memcached
-systemctl enable memcached
-systemctl status memcached
-memcached -p 11211 -U 11111 -u memcached -d
+# Update and install dependencies
+sudo apt update -y
 
-# Rabbit
-yum install socat -y
-yum install erlang -y
-yum install wget -y
-wget https://www.rabbitmq.com/releases/rabbitmq-server/v3.6.10/rabbitmq-server-3.6.10-1.el7.noarch.rpm
-rpm --import https://www.rabbitmq.com/rabbitmq-release-signing-key.asc
-yum update
-rpm -Uvh rabbitmq-server-3.6.10-1.el7.noarch.rpm
-systemctl start rabbitmq-server
-systemctl enable rabbitmq-server
-systemctl status rabbitmq-server
-echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config
-rabbitmqctl add_user rabbit bunny
-rabbitmqctl set_user_tags rabbit administrator
-systemctl restart rabbitmq-server
+# Memcached
+sudo apt install memcached -y
+sudo systemctl start memcached
+sudo systemctl enable memcached
 
-# Mysql
-yum install mariadb-server -y
+# RabbitMQ
+sudo apt install -y curl gnupg apt-transport-https
+curl -fsSL https://packagecloud.io/rabbitmq/rabbitmq-server/gpgkey | sudo apt-key add -
+sudo apt-key adv --keyserver "hkps://keyserver.ubuntu.com:443" --recv-keys F7B8CEA6056E8E56
+sudo tee /etc/apt/sources.list.d/rabbitmq.list <<EOF
+deb https://packagecloud.io/rabbitmq/rabbitmq-server/ubuntu/ focal main
+EOF
+sudo apt update -y
+sudo apt install rabbitmq-server -y
+sudo systemctl start rabbitmq-server
+sudo systemctl enable rabbitmq-server
+echo "[{rabbit, [{loopback_users, []}]}]." | sudo tee /etc/rabbitmq/rabbitmq.config
+sudo rabbitmqctl add_user rabbit bunny
+sudo rabbitmqctl set_user_tags rabbit administrator
+sudo systemctl restart rabbitmq-server
 
-#mysql_secure_installation
-sed -i 's/^127.0.0.1/0.0.0.0/' /etc/my.cnf
+# MariaDB (MySQL)
+sudo apt install mariadb-server -y
 
-# starting & enabling mariadb-server
-systemctl start mariadb
-systemctl enable mariadb
+# Secure MariaDB installation
+sudo sed -i 's/^bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-#restore the dump file for the application
-mysqladmin -u root password "$DATABASE_PASS"
-mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
-mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
-mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
-mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
-mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
-mysql -u root -p"$DATABASE_PASS" -e "create database accounts"
-mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'localhost' identified by 'admin123'"
-mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'app01' identified by 'admin123'"
-mysql -u root -p"$DATABASE_PASS" accounts < /vagrant/vprofile-repo/src/main/resources/db_backup.sql
-mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+# Starting & enabling MariaDB server
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
 
-# Restart mariadb-server
-systemctl restart mariadb
+# Secure MariaDB
+sudo mysqladmin -u root password "$DATABASE_PASS"
+sudo mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+sudo mysql -u root -p"$DATABASE_PASS" -e "create database accounts"
+sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'localhost' identified by 'admin123'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'app01' identified by 'admin123'"
+sudo mysql -u root -p"$DATABASE_PASS" accounts < /vagrant/vprofile-repo/src/main/resources/db_backup.sql
+sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+
+# Restart MariaDB
+sudo systemctl restart mariadb
