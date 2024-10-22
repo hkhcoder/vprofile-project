5c9b06249d23d77d971d38356719d823d8924841
Imran teli <hkhcoder@gmail.com>
2024-10-22 11:18:19 +0530

    refactor br

--- userdata/memcache.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo dnf install memcached -y
-sudo systemctl start memcached
-sudo systemctl enable memcached
-sudo systemctl status memcached
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/sysconfig/memcached
-sudo systemctl restart memcached
-sudo memcached -p 11211 -U 11111 -u memcached -d
--- userdata/tomcat_ubuntu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt upgrade -y
-sudo apt install openjdk-17-jdk -y
-sudo apt install tomcat10 tomcat10-admin tomcat10-docs tomcat10-common git -y
