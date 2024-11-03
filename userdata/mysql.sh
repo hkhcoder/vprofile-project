3ef94bfb030dbaa659f01237adfdef10d32c1418
Harshini1928 <140634362+Harshini1928@users.noreply.github.com>
2024-11-03 12:43:31 +0530

    Updated mysql.sh

--- userdata/mysql.sh
+++ userdata/mysql.sh
@@ -3,12 +3,12 @@ DATABASE_PASS='admin123'
 sudo yum update -y
 #sudo yum install epel-release -y
 sudo yum install git zip unzip -y
-sudo dnf install mariadb105-server -y
+sudo yum install mariadb105-server -y
 # starting & enabling mariadb-server
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 cd /tmp/
-git clone -b main https://github.com/hkhcoder/vprofile-project.git
+git clone -b main https://github.com/Harshini1928/vprofile-project.git
 #restore the dump file for the application
 sudo mysqladmin -u root password "$DATABASE_PASS"
 sudo mysql -u root -p"$DATABASE_PASS" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DATABASE_PASS'"
@@ -21,3 +21,10 @@ sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 
 sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'%' identified by 'admin123'"
 sudo mysql -u root -p"$DATABASE_PASS" accounts < /tmp/vprofile-project/src/main/resources/db_backup.sql
 sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+#Starting the firewall and allowing the mariadb to access from port no.3306
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+sudo firewall-cmd --get-active-zones
+sudo firewall-cmd --zone=public --add-port=3306/tcp --permanent
+sudo firewall-cmd --reload
+sudo systemctl restart mariadb
