d0a493c06aff2126e0426c0ea099346b72c83568
hkhcoder <137309866+hkhcoder@users.noreply.github.com>
2024-11-20 06:45:11 +0530

    Update rabbitmq.sh

--- userdata/rabbitmq.sh
+++ userdata/rabbitmq.sh
@@ -5,7 +5,7 @@ rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/rab
 rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-erlang.E495BB49CC4BBE5B.key'
 ## RabbitMQ server repository
 rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-server.9F4587F226208342.key'
-curl -o /etc/yum.repos.d/rabbitmq.repo https://raw.githubusercontent.com/hkhcoder/vprofile-project/aws-LiftAndShift/al2023rmq.repo
+curl -o /etc/yum.repos.d/rabbitmq.repo https://raw.githubusercontent.com/hkhcoder/vprofile-project/refs/heads/awsliftandshift/al2023rmq.repo
 dnf update -y
 ## install these dependencies from standard OS repositories
 dnf install socat logrotate -y
