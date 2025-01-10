5c91c8d331d9ea8ef00f15bf6142b1d1b002b4ab
Imran teli <hkhcoder@gmail.com>
2025-01-10 10:34:43 +0530

    jdk17 update

--- vagrant/Automated_provisioning_MacOSM1/mysql.sh
+++ vagrant/Automated_provisioning_MacOSM1/mysql.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-sudo mv /etc/yum.repos.d/fedora-updates.repo /tmp/
-sudo mv /etc/yum.repos.d/fedora-updates-modular.repo /tmp/
-sudo yum clean all
-#sudo yum update -y
 DATABASE_PASS='admin123'
-#sudo yum install epel-release -y
+sudo yum update -y
+sudo yum install epel-release -y
 sudo yum install git zip unzip -y
 sudo yum install mariadb-server -y
 
 
 # starting & enabling mariadb-server
-
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 cd /tmp/
 git clone -b main https://github.com/hkhcoder/vprofile-project.git
 #restore the dump file for the application
 sudo mysqladmin -u root password "$DATABASE_PASS"
-#sudo mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
@@ -33,11 +28,9 @@ sudo systemctl restart mariadb
 
 
 #starting the firewall and allowing the mariadb to access from port no. 3306
-#sudo systemctl start firewalld
-#sudo systemctl enable firewalld
-#sudo firewall-cmd --get-active-zones
-#sudo firewall-cmd --zone=public --add-port=3306/tcp --permanent
-#sudo firewall-cmd --reload
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+sudo firewall-cmd --get-active-zones
+sudo firewall-cmd --zone=public --add-port=3306/tcp --permanent
+sudo firewall-cmd --reload
 sudo systemctl restart mariadb
