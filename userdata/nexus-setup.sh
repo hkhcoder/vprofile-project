eafa62355fba6f4f84b6d8c7a0aa3421874ba32c
hkhcoder <137309866+hkhcoder@users.noreply.github.com>
2025-03-05 19:57:26 +0530

    Update nexus-setup.sh

--- userdata/nexus-setup.sh
+++ userdata/nexus-setup.sh
@@ -5,10 +5,10 @@ sudo curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto
 
 sudo yum install -y java-17-amazon-corretto-devel wget -y
 
-mkdir -p /opt/nexus/   
-mkdir -p /tmp/nexus/                           
+mkdir -p /opt/nexus/
+mkdir -p /tmp/nexus/
 cd /tmp/nexus/
-NEXUSURL="https://download.sonatype.com/nexus/3/latest-unix.tar.gz"
+NEXUSURL="https://download.sonatype.com/nexus/3/nexus-unix-x86-64-3.78.0-14.tar.gz"
 wget $NEXUSURL -O nexus.tar.gz
 sleep 10
 EXTOUT=`tar xzvf nexus.tar.gz`
@@ -18,22 +18,22 @@ rm -rf /tmp/nexus/nexus.tar.gz
 cp -r /tmp/nexus/* /opt/nexus/
 sleep 5
 useradd nexus
-chown -R nexus.nexus /opt/nexus 
+chown -R nexus.nexus /opt/nexus
 cat <<EOT>> /etc/systemd/system/nexus.service
-[Unit]                                                                          
-Description=nexus service                                                       
-After=network.target                                                            
-                                                                  
-[Service]                                                                       
-Type=forking                                                                    
-LimitNOFILE=65536                                                               
-ExecStart=/opt/nexus/$NEXUSDIR/bin/nexus start                                  
-ExecStop=/opt/nexus/$NEXUSDIR/bin/nexus stop                                    
-User=nexus                                                                      
-Restart=on-abort                                                                
-                                                                  
-[Install]                                                                       
-WantedBy=multi-user.target                                                      
+[Unit]
+Description=nexus service
+After=network.target
+
+[Service]
+Type=forking
+LimitNOFILE=65536
+ExecStart=/opt/nexus/$NEXUSDIR/bin/nexus start
+ExecStop=/opt/nexus/$NEXUSDIR/bin/nexus stop
+User=nexus
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.target
 
 EOT
 
