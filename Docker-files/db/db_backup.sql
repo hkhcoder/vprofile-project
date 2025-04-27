8833773fcc69ab9effd10301eb1bd073e049ecca
ajithkumars3131 <ajithkumar.s3131@gmail.com>
2025-04-27 17:05:55 +0000

    checkinglease enter the commit message for your changes. Lines starting

--- Docker-files/db/db_backup.sql
+++ Docker-files/db/db_backup.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `role`
 --
-#
+
 DROP TABLE IF EXISTS `role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
