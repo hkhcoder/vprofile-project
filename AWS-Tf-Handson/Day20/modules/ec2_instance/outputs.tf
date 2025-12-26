a794dc44c8f084e3365c810d74479a7c9d7ef7ff
Ankur Prakash <ankur.gla@gmail.com>
2025-12-26 08:04:23 +0530

    Commit

--- AWS-Tf-Handson/Day20/modules/ec2_instance/outputs.tf
+++ AWS-Tf-Handson/Day20/modules/ec2_instance/outputs.tf
@@ -1,8 +1,8 @@
 output "public_ip" {
   description = "Public IP of the created instance"
-  value       = aws_instance.this.public_ip
+  value       = aws_instance.name.public_ip
 }
 
 output "instance_id" {
-  value = aws_instance.this.id
+  value = aws_instance.name.id
 }
