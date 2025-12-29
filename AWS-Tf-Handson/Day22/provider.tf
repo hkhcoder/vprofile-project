9b81d57323889840f72422e3c88f4b52fa84dcb8
Ankur Prakash <ankur.gla@gmail.com>
2025-12-29 08:18:30 +0530

    Commit modules & provider.tf

--- AWS-Tf-Handson/Day22/provider.tf
+++ AWS-Tf-Handson/Day22/provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
 provider "aws" {
   region  = var.region # Replace with your desired AWS region
   profile = "468284643560"
