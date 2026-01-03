205fe02993148d0318974d642b20ee8641805d0c
Sheetal A <sheetalgagrahari@gmail.com>
2026-01-03 19:14:43 +0100

    Ignore unwanted garbage files

--- src/main/java/com/visualpathit/account/service/SecurityService.java
+++
@@ -1,9 +0,0 @@
-package com.visualpathit.account.service;
-
-/** method for finding already added user !*/
-public interface SecurityService {
-	/** {@inheritDoc}} !*/
-    String findLoggedInUsername();
-
-    boolean autologin(String username, String password);
-}
